8f22cff49151c8b6e59355f7e9d305556f42b515
Heidi Einholz <heidein5@gmail.com>
2019-11-10 20:14:38 -0700

    Progress on all

--- script.js
+++ script.js
@@ -1,29 +1,87 @@
-//define variables
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-var chars = ["~", "@", "#", "%", "^", "&", "*", "(", ")", "-", "+", "=", ">", "<", "?"]
+//define character variables
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["~", "@", "#", "%", "^", "&", "*", "(", ")", "-", "+", "=", ">", "<", "?"];
+var passwordCharacters = [];
+var password = "";
+//ask the user how long they want their password
+var passwordLength;
 
-
-// Generate Password button
-function GetRandom()
-    {
-        var myElement = document.getElementById("passwordfield")
-        myElement.value = Math.random()
+function gatherSymbols() {
+    var passwordLength = prompt("Enter a number between 8 and 128 for how long you would like your password to be");
+    console.log("passwordLength: ", passwordLength);
+    };
+    //make sure the user chooses a valid password length
+    if(passwordLength < 8 || passwordLength > 128){
+        passwordLength = prompt("Please enter a password length between 8 and 128 characters");
+        console.log("passwordLength: ", passwordLength);
     }
-// Copy to clipboard button
-function myFunction() {
-    /* Get the text field */
-    var copyText = document.getElementById("myInput");
-  
-    /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-  
-    /* Copy the text inside the text field */
-    document.execCommand("copy");
-  
-    /* Alert the copied text */
-    alert("Copied the text: " + copyText.value);
-  }
+    //establish the types of characters in password
+    var useLowercase = confirm("Do you want to use lowercase letters in your password?");
+    console.log("useLowercase: ", useLowercase);
+    var useUppercase = confirm("Do you want to use uppercase letters in your password");
+    console.log("useUppercase: ", useUppercase);
+    var useNumbers = confirm("Do you want numbers in your password?");
+    console.log("useNumbers ", useNumbers);
+    var useSpecial = confirm("Do you want to use special characters in your password?");
+    console.log("useSpecial: ", useSpecial);
+    //if they do not choose any type of character reprompt user to choose at least one type of character for their password
+    while(useLowercase === false && useUppercase === false && useNumbers === false && useSpecial === false){
+        useNumbers = confirm("Please choose at least one type of character. How about numbers?");
+        console.log("useNumbers: ", useNumbers);
+        useLowercase = confirm("How about lowercase letters?");
+        console.log("useLowercase: ", useLowercase);
+        useUppercase = confirm("How about uppercase letters?");
+        console.log("useUppercase: ", useUppercase);
+        useSpecial = confirm("How about special characters?");
+        console.log("useSpecial: ", useSpecial);
+    }
 
+    //user chooses lowercase letters
+    if(useLowercase === true) {
+        passwordCharacters = passwordCharacters.concat(lowerCase);
+        console.log("passwordCharacters: ", passwordCharacters);
+    }
+    //user chooses uppercase letters
+    if(useUppercase === true) {
+        passwordCharacters = passwordCharacters.concat(upperCase);
+        console.log("passwordCharacters: ", passwordCharacters);
+    }
+    //user chooses numbers
+    if(useNumbers === true) {
+        passwordCharacters = passwordCharacters.concat(numbers);
+        console.log("passwordCharacters: ", passwordCharacters);
+    }
+    //user chooses special characters
+    if(useSpecial === true) {
+        passwordCharacters = passwordCharacters.concat(specialChar);
+        console.log("passwordCharacters: ", passwordCharacters);
+    } 
+
+    function createPassword(passwordLength, passwordCharacters){
+        for(i = 0; i < passwordLength; i++){
+            //generates random character from the array
+            var character = passwordCharacters[Math.floor(Math.random()*passwordCharacters.length)];
+            console.log("character: ", character)
+            //adds the randomly generated character to the password
+            password = password + character;
+            console.log("password: ", password);
+    }
+        var password = alert("Your password is" + password);
+
+}
+
+gatherSymbols();
+createPassword(passwordLength, passwordCharacters);
+    passwordBox.textContent = password;
+
+    //generates new password 
+    btn1.addEventListener("click", function(){
+    //reset password and password character options
+    password = "";
+    passwordCharacters = [];
+    gatherSymbols();
+    createPassword(passwordLength, passwordCharacters);
+    passwordBox.textContent = password;
+})
